d459eacd1c2d4fae1f3f9006efe4654306460673
Kuryashka <78092503+Kuryashka@users.noreply.github.com>
2021-03-03 20:44:58 +0200

    Update text.py

--- exam1/text.py
+++ exam1/text.py
@@ -1,3 +1,10 @@
-text = "The core of extensible programming is defining functions"
-a = text[::2]
+text = 'The core of extensible programming is defining functions.'
+a = text.replace(' ', '')
+b = 1
+for i in a:
+    if b % 2 == 0:
+        print(i)
+        b += 1
+    else:
+        b += 1
 print(a)
